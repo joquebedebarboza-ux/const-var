789a972797ba68d09200e23f4148348540e32f12
joquebedebarboza-ux <joquebedebarboza@gmail.com>
2026-02-20 17:09:35 -0300

    Aula var e const

---
+++ var_const.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Const_var</title>
+</head>
+<body>
+    <?php 
+    /*int = numero, 
+    string = caracere, 
+    boleano = true/ false,
+    float = 0.0 */
+    
+ $aluno = "Arnoldo shuaseneguer"; 
+ $idade = "130";
+
+
+ /*const inicia com a palavra define
+enre aspas duplas
+1° consante
+2° valor */
+
+
+define ("instituicao", "centro de educacao tec. do amazonas");
+
+
+echo "Ola aluno, $aluno $idade bem vindo ao cetam" . instituicao 
+
+
+
+
+
+    ?>
+</body>
+</html>
